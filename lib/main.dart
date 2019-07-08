28f78012205bc20d0e5c657362c8e16845368943
Veli Bacık <velibacik60@gmail.com>
2019-07-08 22:33:15 +0300

    last changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hwatutorial/httpHello.dart';
+import 'package:hwatutorial/shadow.dart';
 import 'package:hwatutorial/tabbar_view.dart';
 import 'package:hwatutorial/views/json_placeholder_view.dart';
 import 'package:hwatutorial/views/uibase.dart';
-import 'package:hwatutorial/widgets/shadow.dart';
 
 void main() => runApp(MyApp());
 
