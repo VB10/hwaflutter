7aaf65123f286da64770a9dd8e70c6276aca95b0
Veli <Bacik>
2019-08-26 23:37:38 +0300

    sliver add

---
+++ lib/views/sliver_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class SliverView extends StatefulWidget {
+  @override
+  _SliverViewState createState() => _SliverViewState();
+}
+
+class _SliverViewState extends State<SliverView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 200,
+            floating: true,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Hello"),
+              centerTitle: false,
+              background: Image.network(
+                "https://picsum.photos/400",
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 100,
+            ),
+          ),
+          SliverGrid.count(
+            crossAxisCount: 5,
+            children: <Widget>[
+              Text("a"),
+              Text("a"),
+              Text("a"),
+              Text("a"),
+              Text("a"),
+               Text("a"),
+              Text("a"),
+              Text("a"),
+              Text("a"),
+              Text("a"),
+               Text("a"),
+              Text("a"),
+              Text("a"),
+              Text("a"),
+              Text("a"),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((index, context) {
+              return Container(
+                color: Colors.red,
+                height: 100,
+                child: Card(child: Text("a")),
+              );
+            }, childCount: 10),
+          )
+        ],
+      ),
+    );
+  }
+}
